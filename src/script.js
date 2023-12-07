872c92584774828145187a763e52ac2d19eebf99
joanamorais13 <joanamorais13@gmail.com>
2023-12-07 11:45:00 +0000

    JS - injected weather data

---
+++ src/script.js
@@ -0,0 +1,55 @@
+function refreshWeather(response){
+  let temperatureElement = document.querySelector("#current-temperature")
+  let temperature = response.data.temperature.current;
+  let cityElement =document.querySelector("#weather-app-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date= new Date(response.data.time*1000);
+
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML= math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHMTL = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date)}
+  //timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()}`
+
+
+  function formatDate(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+    
+    if (minutes < 10) {
+        minutes =`0${minutes}`
+    }
+   if (hours < 10) {
+        hours =`0${hours}`
+    }
+
+    return `${day} ${hours}:${minutes}`
+  }
+
+function searchCity(city){
+let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+
+}
+
+function handleSearchSubmit(event){
+event.preventDefault();
+let searchInput = document.querySelector("#search-form-input");
+
+searchCity(searchInput.value)
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Porto");
