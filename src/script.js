d754f4b74f13cb7ae39e9066ce3c4bcea9350320
joanamorais13 <joanamorais13@gmail.com>
2023-12-12 14:59:08 +0000

    pushed some CSS updates

--- src/script.js
+++ src/script.js
@@ -1,22 +1,25 @@
+function capitalize(value) {
+    return value.charAt(0).toUpperCase() + value.slice(1);
+}
+
 function refreshWeather(response){
   let temperatureElement = document.querySelector("#current-temperature")
   let temperature = response.data.temperature.current;
-  let cityElement =document.querySelector("#weather-app-city");
+  let cityElement = document.querySelector("#weather-app-city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date= new Date(response.data.time*1000);
-
-
+  let iconElement = document.querySelector ("#icon");
+  temperatureElement.innerHTML= Math.round(temperature);
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML= math.round(temperature);
-  descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHMTL = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = capitalize(response.data.condition.description);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
-  timeElement.innerHTML = formatDate(date)}
-  //timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()}`
-
+  timeElement.innerHTML = formatDate(date);
+  iconElement.innerHMTL = `<img src="${response.data.condition.icon_url}"/>`
+}
 
   function formatDate(date){
     let minutes = date.getMinutes();
@@ -36,7 +39,7 @@ function refreshWeather(response){
 
 function searchCity(city){
 let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 axios.get(apiUrl).then(refreshWeather);
 
 }
